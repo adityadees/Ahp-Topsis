118d1e00cd92d8b1c826a54180e32dfb5803331c
AdityaDS <adityads@ymail.com>
2019-05-14 03:33:48 +0700

    ahp

--- application/views/backend/layout/topbar.php
+++ application/views/backend/layout/topbar.php
@@ -9,8 +9,8 @@
         </li>
         <li class="nav-item mr-auto">
           <a class="navbar-brand" href="<?= base_url();?>admin">
-            <img class="brand-logo" alt="ORM FLORIST" src="<?= base_url();?>assets/images/logo/thumbnail.png" width="30px" height="25px">
-            <h2 class="brand-text">ORM</h2>
+           <!--  <img class="brand-logo" alt="AHP - Topsis" src="<?= base_url();?>assets/images/logo/thumbnail.png" width="30px" height="25px"> -->
+            <h2 class="brand-text">AHP-TOPSIS</h2>
           </a>
         </li>
         <li class="nav-item d-none d-md-block nav-toggle">
